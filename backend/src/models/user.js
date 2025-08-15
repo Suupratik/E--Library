669e88933230a815cb08828caf17a5b4542d43ff
Supratik <sriijan6@gmail.com>
2025-08-15 09:00:29 +0530

    Final backend code

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,12 +1,24 @@
-const { model, Schema } = require("mongoose")
+const { model, Schema } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserModel = model(
-  "users",
-  new Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true },
-  })
-)
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+});
 
-module.exports = { UserModel }
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare entered password with stored hash
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+const UserModel = model("users", userSchema);
+
+module.exports = { UserModel };
